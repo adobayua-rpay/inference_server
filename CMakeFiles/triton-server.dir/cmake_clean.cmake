99303945632a366309cdb06903ec1f204099cfc9
AlphaRora <adoba@alpharora.com>
2024-03-11 18:52:30 +0000

    ha

---
+++ CMakeFiles/triton-server.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/triton-server"
+  "CMakeFiles/triton-server-complete"
+  "triton-server/src/triton-server-stamp/triton-server-build"
+  "triton-server/src/triton-server-stamp/triton-server-configure"
+  "triton-server/src/triton-server-stamp/triton-server-download"
+  "triton-server/src/triton-server-stamp/triton-server-install"
+  "triton-server/src/triton-server-stamp/triton-server-mkdir"
+  "triton-server/src/triton-server-stamp/triton-server-patch"
+  "triton-server/src/triton-server-stamp/triton-server-update"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/triton-server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
